b51c456474a6ee7581ba6e00cb0acaea653dcb57
Hernan Diaz <hernan@pollen.com.au>
2024-12-03 11:40:30 +1100

    center card

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import HueRotationDemo from "./pages/hue-rotation-demo";
 function App() {
   return (
     <>
-      <HueRotationDemo />
+      <main className="py-12 flex flex-col items-center justify-center">
+        <HueRotationDemo />
+      </main>
     </>
   );
 }
